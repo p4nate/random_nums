9544f70244a4ccea2feabeca1ce07861f820be22
Nathan Eror <derby24@gmail.com>
2016-04-27 00:46:39 -0700

    battery about to die

--- lotto.rb
+++ lotto.rb
@@ -1,17 +1,31 @@
 
 puts "Hello world"
-prng = Random.new
 
 numsA = {1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0, 13 => 0, 14 => 0, 15 => 0, 16 => 0, 17 => 0, 18 => 0, 19 => 0, 20 => 0, 21 => 0, 22 => 0, 23 => 0, 24 => 0, 25 => 0, 26 => 0, 27 => 0, 28 => 0, 29 => 0, 30 => 0, 31 => 0, 32 => 0, 33 => 0, 34 => 0, 35 => 0, 36 => 0, 37 => 0, 38 => 0, 39 => 0, 40 => 0, 41 => 0, 42 => 0, 43 => 0, 44 => 0, 45 => 0, 46 => 0, 47 => 0, 48 => 0, 49 => 0} 
+missed = Array.new
+s = "test"
 
-6.times do
+prng = Random.new
+
+1000.times do
     k = prng.rand(1..49)
-    puts k 
     if numsA.select {|k,v| k.equal? k} then
-	numsA[k] += 1
-        puts "have a match #{numsA[k]}"
-    else
-        puts "didn't match any hask keys\n"
+        numsA[k] += 1
+        missed.push(s)
+    else 
+        puts missed.push(k) 
     end
 end
 
+if missed.empty? then
+    puts missed
+    puts "im mt"
+else
+    puts "not mt"
+end
+
+if true == true then
+    puts "it is"
+end
+
+puts numsA
