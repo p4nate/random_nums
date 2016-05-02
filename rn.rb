98c48acc4435be0ae4a666781deb2aa02e6ba0f0
Nathan Eror <derby24@gmail.com>
2016-05-01 23:37:56 -0700

    add support for rounds of runs

--- rn.rb
+++ rn.rb
@@ -1,17 +1,25 @@
 #!/usr/bin/ruby
 
-$numsA = {1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0, 13 => 0, 14 => 0, 15 => 0, 16 => 0, 17 => 0, 18 => 0, 19 => 0, 20 => 0, 21 => 0, 22 => 0, 23 => 0, 24 => 0, 25 => 0, 26 => 0, 27 => 0, 28 => 0, 29 => 0, 30 => 0, 31 => 0, 32 => 0, 33 => 0, 34 => 0, 35 => 0, 36 => 0, 37 => 0, 38 => 0, 39 => 0, 40 => 0, 41 => 0, 42 => 0, 43 => 0, 44 => 0, 45 => 0, 46 => 0, 47 => 0, 48 => 0, 49 => 0} 
+def ha
+    $numsA = {1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0, 13 => 0, 14 => 0, 15 => 0, 16 => 0, 17 => 0, 18 => 0, 19 => 0, 20 => 0, 21 => 0, 22 => 0, 23 => 0, 24 => 0, 25 => 0, 26 => 0, 27 => 0, 28 => 0, 29 => 0, 30 => 0, 31 => 0, 32 => 0, 33 => 0, 34 => 0, 35 => 0, 36 => 0, 37 => 0, 38 => 0, 39 => 0, 40 => 0, 41 => 0, 42 => 0, 43 => 0, 44 => 0, 45 => 0, 46 => 0, 47 => 0, 48 => 0, 49 => 0} 
+end
 
+ha
 $runs = ARGV[0]
+$rounds = ARGV[1]
 $missed = Array.new
 $prng = Random.new
 
-def getArg
+def getArgs
     if $runs.nil? || $runs == 0 then
-        puts "How many times do you want to run? ./rn.rb [ n ] times"
+        puts "How many times per run do you want to run? ./rn.rb [ n ] times"
 	exit
     else
         $runs = ARGV[0]
+    end
+     if $rounds.nil? || $rounds == 0 then
+        print "running one round of runs\n"
+        $rounds = 1
     end
 end	
 
@@ -26,10 +34,18 @@ def countm
     end
 end
 
-getArg
+def ocountm
+    puts Hash[$numsA.sort_by{|k, v| v}.reverse[0..6]]
+    ha
+end
 
-$runs.to_i.times do
-   countm
+getArgs
+
+$rounds.to_i.times do
+    $runs.to_i.times do
+        countm
+    end
+    ocountm
 end
 
 if $missed.empty? then
@@ -39,8 +55,4 @@ else
     puts $missed
 end
 
-if true == true then
-    puts "it is"
-end
 
-puts Hash[$numsA.sort_by{|k, v| v}.reverse]
