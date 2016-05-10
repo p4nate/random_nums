65b85bcd8cb23dccf74122d645792a74650e2d0c
Nathan Eror <derby24@gmail.com>
2016-05-09 22:29:49 -0700

    do it

--- rn.rb
+++ rn.rb
@@ -6,6 +6,7 @@ def ha
     $numsA = {1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0, 13 => 0, 14 => 0, 15 => 0, 16 => 0, 17 => 0, 18 => 0, 19 => 0, 20 => 0, 21 => 0, 22 => 0, 23 => 0, 24 => 0, 25 => 0, 26 => 0, 27 => 0, 28 => 0, 29 => 0, 30 => 0, 31 => 0, 32 => 0, 33 => 0, 34 => 0, 35 => 0, 36 => 0, 37 => 0, 38 => 0, 39 => 0, 40 => 0, 41 => 0, 42 => 0, 43 => 0, 44 => 0, 45 => 0, 46 => 0, 47 => 0, 48 => 0, 49 => 0} 
 end
 
+logDir = './data'
 ha
 $runs = ARGV[0]
 $rounds = ARGV[1]
@@ -44,7 +45,16 @@ def ocountm
     puts Hash[$numsA.sort_by{|k, v| v}.reverse[0..6]]
 end
 
+def wlog
+    t = Time.now
+    ext = '.log'
+    puts t
+    tt = [t.year, t.month,t.day,t.hour,t.min,t.sec,ext].join
+    puts tt
+end
+
 getArgs
+wlog
 
 $rounds.to_i.times do
     $runs.to_i.times do
@@ -59,5 +69,3 @@ else
     puts "these fell out"
     puts $missed
 end
-
-
